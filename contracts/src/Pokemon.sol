6bafcbc2fcbd491f8fd2bdde509d44e526fde8ba
Luiz <lvj253@gmail.com>
2024-09-07 19:35:22 -0300

    pokemon

--- contracts/src/Pokemon.sol
+++ contracts/src/Pokemon.sol
@@ -4,12 +4,70 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./PokemonSource.sol";
 
-contract Pokemon is ERC721 {
+contract Pokemon is ERC721, VRFConsumerBaseV2Plus {
 
     PokemonSource public pokemonSource;
+
+    uint256 public counter;
+
+    mapping(uint256 => uint256) public pokemonToHp;
+    mapping(uint256 => uint256) public pokemonToAttack;
+    mapping(uint256 => uint256) public pokemonToDefense;
+    mapping(uint256 => uint256) public pokemonToSpeed;
+
+
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+        public s_requests; /* requestId --> requestStatus */
+
+    // Your subscription ID.
+    uint256 public s_subscriptionId;
+
+    // Past request IDs.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // The gas lane to use, which specifies the maximum gas price to bump to.
+    // For a list of available gas lanes on each network,
+    // see https://docs.chain.link/docs/vrf/v2-5/supported-networks
+    bytes32 public keyHash =
+        0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 public callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 public requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2_5.MAX_NUM_WORDS.
+    uint32 public numWords = 2;
+
+    /**
+     * HARDCODED FOR SEPOLIA
+     * COORDINATOR: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B
+     */
 
     constructor(address _pokemonSource) ERC721("Pokemon", "PK") {
         pokemonSource = PokemonSource(_pokemonSource);
     }
+
+
+    function requestPokemon() public payable returns (uint256 requestId)
+
+
+
 
 }
--- contracts/src/PokemonSource.sol
+++ contracts/src/PokemonSource.sol
@@ -2,11 +2,60 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 contract PokemonSource is ERC721, Ownable {
+
+    uint256 public counter;
+
+    mapping(uint256 => uint256) public pokemonToHpMax;
+    mapping(uint256 => uint256) public pokemonToHpMin;
+    mapping(uint256 => uint256) public pokemonToAttackMax;
+    mapping(uint256 => uint256) public pokemonToAttackMin;
+    mapping(uint256 => uint256) public pokemonToDefenseMax;
+    mapping(uint256 => uint256) public pokemonToDefenseMin;
+    mapping(uint256 => uint256) public pokemonToSpeedMax;
+    mapping(uint256 => uint256) public pokemonToSpeedMin;
+
+    mapping(uint256 => string[]) public pokemonToPossibleTypes;
+    mapping(uint256 => uint256[]) public pokemonToPossibleMoves;
     
 
-    constructor() ERC721("PokemonSource", "PS") {
+    constructor(address _owner)  Ownable(_owner) ERC721("PokemonSource", "PS"){
 
     }
+
+    function createPokemonSource(
+        uint256 _hpMax,
+        uint256 _hpMin,
+        uint256 _attackMax,
+        uint256 _attackMin,
+        uint256 _defenseMax,
+        uint256 _defenseMin,
+        uint256 _speedMax,
+        uint256 _speedMin,
+        string[] memory _possibleTypes,
+        uint256[] memory _possibleMoves
+    ) public onlyOwner {
+        counter++;
+        pokemonToHpMax[counter] = _hpMax;
+        pokemonToHpMin[counter] = _hpMin;
+        pokemonToAttackMax[counter] = _attackMax;
+        pokemonToAttackMin[counter] = _attackMin;
+        pokemonToDefenseMax[counter] = _defenseMax;
+        pokemonToDefenseMin[counter] = _defenseMin;
+        pokemonToSpeedMax[counter] = _speedMax;
+        pokemonToSpeedMin[counter] = _speedMin;
+        pokemonToPossibleTypes[counter] = _possibleTypes;
+        pokemonToPossibleMoves[counter] = _possibleMoves;
+
+        _mint(address(this), counter);
+    }
+
+
+    function getRandomPokemonId() public view returns (uint256) {
+        
+    }
+
 }
